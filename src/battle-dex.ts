6bd59bcc54d3e97c548cab480ad809084e27fdc2
Anders Hoffman <pccompositor@aim.com>
2022-03-30 23:02:03 -0500

    Update battle-dex.ts

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -367,7 +367,7 @@ const Dex = new class implements ModdedDex {
 		let id = toID(nameOrSpecies);
 		let formid = id;
 		if (!window.BattlePokedexAltForms) window.BattlePokedexAltForms = {};
-		if (formid in window.BattlePokedexAltForms) return window.BattlePokedexAltForms[formid];
+		if (formid in window.BattlePokedexAltForms) return window.[BattlePokedexAltFormsformid];
 		if (window.BattleAliases && id in BattleAliases) {
 			name = BattleAliases[id];
 			id = toID(name);
@@ -381,7 +381,12 @@ const Dex = new class implements ModdedDex {
 		}
 		if (!window.BattlePokedex) window.BattlePokedex = {};
 		let data = window.BattlePokedex[id];
-
+		if (!data) {
+			if (window.room.curTeam.mod) data = this.moddedDexes[window.curTeam.mod].getSpecies();
+			else console.log("couldn't find mod");
+		}
+		
+		
 		let species: Species;
 		if (data && typeof data.exists === 'boolean') {
 			species = data;
