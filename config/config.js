222bb00e369c74e825f00e24fd614c4f2516499a
kevin <sunfished@gmail.com>
2023-05-06 17:56:22 -0700

    Init

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'dragonheaven',
-	host: 'localhost',
+	host: '76.50.33.22',
 	port: 8000,
 	httpport: 8000,
 	altport: 80,
