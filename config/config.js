40ac305a84df7dcc5bdd59f648998c8bbf520749
scoopapa <pccompositor@aim.com>
2023-05-11 16:53:58 -0500

    update

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (6f0d31d0)";
+Config.version = "0.11.2 (cd4e622c)";
 
 Config.routes = {
 	root: 'dragonheavenserver.herokuapp.com',
